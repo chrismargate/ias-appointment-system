c7e33445bce3a7dc2fa38c19371ed70042865c37
chris <chris@SkyMargs.mshome.net>
2022-07-07 19:00:40 +0800

    modified settings.py

--- acare/urls.py
+++ acare/urls.py
@@ -17,19 +17,19 @@ from django.contrib import admin
 from django.urls import path, include
 
 # all modules below have been added from tutorial
-from django.conf import settings
-from django.conf.urls.static import static
+# from django.conf import settings
+# from django.conf.urls.static import static
 
-from django.views.static import serve
-from django.conf.urls.static import url
+# from django.views.static import serve
+# from django.conf.urls.static import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("main.urls")),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    # url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    # url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
-urlpatterns=urlpatterns+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+# urlpatterns=urlpatterns+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 # + static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
 # from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 # urlpatterns += staticfiles_urlpatterns()
