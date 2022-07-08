c63bfb63be47b6c381e5c7301e84a8e7d86c6656
margate23 <78247006+chrismargo@users.noreply.github.com>
2022-07-09 03:03:29 +0800

    configured procfile

--- acare/settings.py
+++ acare/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-# import django_heroku
+import django_heroku
+import dj_database_url
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -124,6 +125,7 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
 
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
