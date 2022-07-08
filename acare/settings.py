ac640284362f440c90fdd228660042cb00595d5c
margate23 <78247006+chrismargo@users.noreply.github.com>
2022-07-09 03:50:07 +0800

    edits

--- acare/settings.py
+++ acare/settings.py
@@ -120,8 +120,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
 
-# PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 # import django_on_heroku
 # django_on_heroku.settings(locals())
