fe68cd5aee18cd64d2781c94f69de40806628c61
margate23 <78247006+chrismargo@users.noreply.github.com>
2022-07-09 03:15:50 +0800

    removed dj_database_url module from import

--- acare/settings.py
+++ acare/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 # import django_heroku
-import dj_database_url
+# import dj_database_url
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
