71871c0b2484f4cb8cba207d880e6071ce65ef2b
margate23 <78247006+chrismargo@users.noreply.github.com>
2022-07-09 03:06:37 +0800

    fixed django-heroku module issue

--- acare/settings.py
+++ acare/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$2#80tciso!0uhjf%ol*24$x45%r&plp!*0$55qo1g-fczc%=^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # ALLOWED_HOSTS = []
 ALLOWED_HOSTS = ['127.0.0.1','acaresystem.herokuapp.com']
